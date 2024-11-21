c527aeb6063006efef63fbdfc19c44b6138247be
yitayew123 <yitayewsolomon3@gmail.com>
2024-11-21 12:43:33 +0300

    File Updated

--- resources/views/Demo.blade.php
+++ resources/views/Demo.blade.php
@@ -1,3 +1,10 @@
+<h1>Well Come To Bladding Example</h1>
+<h2>Well Come {{$name}}</h2>
+<h3>Well Come User {{print_r($family)}}</h3>
+<h4>Well Come User {{print_r($family[0])}}</h4>
+
+
+
 <x-message-banner msg="user added seccesfully" class="succ"/>
 <x-message-banner msg="user deleted seccesfully" class="del"/>
 <x-message-banner msg="you are not successfull" class="error"/>
